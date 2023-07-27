6e37cb8bf1a9ec3f378a95d9195821fc7b572bfe
Thulasiram <thulasi29n@gmail.com>
2023-07-27 23:31:00 +0100

    initial

--- A.java
+++ A.java
@@ -23,3 +23,60 @@ for (int i = 0; i < regulators.length; i++) {
 
 Map<String, KStream<String, ValidatedTransaction>> branchedStreams = repartitionedStream
         .split(branches);
+
+
+public class BatchTrackingProcessor implements Processor<String, EnrichedTransaction> {
+
+    private final Duration batchDuration;
+    private final int maxBatchSize;
+    private final String stateStoreName;
+
+    private ProcessorContext context;
+    private KeyValueStore<String, List<EnrichedTransaction>> stateStore;
+    private List<EnrichedTransaction> currentBatch;
+    private int batchSize;
+
+    public BatchTrackingProcessor(Duration batchDuration, int maxBatchSize) {
+        this.batchDuration = batchDuration;
+        this.maxBatchSize = maxBatchSize;
+        this.stateStoreName = "stateStore";
+        this.currentBatch = new ArrayList<>();
+        this.batchSize = 0;
+    }
+
+    @Override
+    public void init(ProcessorContext context) {
+        this.context = context;
+        this.stateStore = (KeyValueStore<String, List<EnrichedTransaction>>) context.getStateStore(stateStoreName);
+        
+        this.context.schedule(batchDuration, PunctuationType.WALL_CLOCK_TIME, timestamp -> {
+            if (!currentBatch.isEmpty()) {
+                // Add batch marker and reset the batch size
+                currentBatch.add(new EnrichedTransaction(true));
+                context.forward(null, currentBatch);
+                currentBatch = new ArrayList<>();
+                batchSize = 0;
+            }
+        });
+    }
+
+    @Override
+    public void process(String key, EnrichedTransaction value) {
+        currentBatch.add(value);
+        batchSize++;
+
+        // If the batch size has reached the max, forward the batch
+        if (batchSize >= maxBatchSize) {
+            // Add batch marker and reset the batch size
+            currentBatch.add(new EnrichedTransaction(true));
+            context.forward(null, currentBatch);
+            currentBatch = new ArrayList<>();
+            batchSize = 0;
+        }
+    }
+
+    @Override
+    public void close() {
+        // Handle the cleanup
+    }
+}
