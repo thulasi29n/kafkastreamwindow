f01f8fe324db050c7da61865fc35e3e89bf90198
Thulasiram <thulasi29n@gmail.com>
2023-07-28 13:24:53 +0100

    initial

---
+++ aatopology.java
@@ -0,0 +1,33 @@
+TopologyBuilder builder = new TopologyBuilder();
+
+// Assuming Regulator is the enum of regulators
+Regulator[] regulators = Regulator.values();
+
+KStream<String, ValidatedTransaction> inputStream = builder.stream("validatedtransaction");
+KStream<String, ValidatedTransaction> repartitionedStream = inputStream.selectKey((key, value) -> value.getRegulatorID());
+
+List<Branched<KStream<String, ValidatedTransaction>>> branches = 
+    Stream.of(regulators).map(regulatorId -> 
+        Branched.<String, ValidatedTransaction>as(regulatorId.toString())
+            .withFunction(stream -> stream
+                .join(globalKTable,
+                    (key, value) -> key,
+                    (validatedTransaction, referenceData) -> {
+                        // Enrich the ValidatedTransaction with reference data from the globalKTable
+                        // You can extract the countSize and triggerTime for each regulator from the reference data
+                        int countSize = referenceData.getCountSize();
+                        LocalTime triggerTime = referenceData.getTriggerTime();
+
+                        EnrichedTransaction enrichedTransaction = new EnrichedTransaction();
+                        // ... Enrichment logic ...
+                        enrichedTransaction.setProcessor(new BatchTrackingProcessor(countSize, triggerTime));
+
+                        return enrichedTransaction;
+                    })
+                .process(() -> value -> value.getProcessor())
+                .to("outputTopic_" + regulatorId.toString(), Produced.with(Serdes.String(), new EnrichedTransactionSerde())))
+    )
+    .collect(Collectors.toList());
+
+Map<String, KStream<String, ValidatedTransaction>> branchedStreams = repartitionedStream
+        .split(branches.toArray(new Branched[0]));
